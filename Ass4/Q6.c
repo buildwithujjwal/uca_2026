35ff8aadb5d2da4b6fca73553f19d1bf4d962864
ujjwal <ujjwal1748.becse24@chitkara.edu.in>
2026-07-31 15:17:52 +0000

    shift x to the right by n using logical shift by implementing the logicalShift function.

---
+++ Ass4/Q6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int localShift(int x, int n) {
+    
+    x >>= n;
+    int mask = (  ~(1 << 31)  >> n) << 1;
+    return x & mask;
+    
+
+}
+
+int main() {
+
+    int x = 0x87654321, n = 4;
+    printf( "0x%08x\n", localShift(x, n) );
+    return 0;
+}
+
